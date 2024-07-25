3a3ee2697ee1682e9c75a429fa4432dde3f34d78
Tshepang <tshepangmog10@gmail.com>
2024-07-26 00:50:06 +0200

    Implemented the collections pages

--- src/app/equipment/page.tsx
+++ src/app/equipment/page.tsx
@@ -1,3 +1,23 @@
+import ProductCard from "@/components/custom-reusable/products/ProductCard";
+import { Separator } from "@/components/ui/separator";
+import { dummyProductsData } from "@/lib/dummy-data";
+import { CollectionsEnum } from "@/lib/types/product-helpers";
+
 export default function Equipment() {
-  return <div>Equipment</div>;
+  const filteredProducts = dummyProductsData.filter((product) => product.collection === CollectionsEnum.EQUIPMENT);
+
+  return (
+    <div className="flex-1 flex flex-col gap-8">
+      <div className="w-full space-y-8">
+        <h1 className="font-suezOne text-3xl">Equipment</h1>
+
+        <Separator className="w-full" />
+      </div>
+      <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/shoes/page.tsx
+++ src/app/shoes/page.tsx
@@ -1,3 +1,23 @@
-export default function Shoes() {
-  return <div>Shoes</div>;
+import ProductCard from "@/components/custom-reusable/products/ProductCard";
+import { Separator } from "@/components/ui/separator";
+import { dummyProductsData } from "@/lib/dummy-data";
+import { CollectionsEnum } from "@/lib/types/product-helpers";
+
+export default function Gift() {
+  const filteredProducts = dummyProductsData.filter((product) => product.collection === CollectionsEnum.SHOES);
+
+  return (
+    <div className="flex-1 flex flex-col gap-8">
+      <div className="w-full space-y-8">
+        <h1 className="font-suezOne text-3xl">Shoes</h1>
+
+        <Separator className="w-full" />
+      </div>
+      <div className="flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
